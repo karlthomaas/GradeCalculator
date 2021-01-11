67e244a4b4802476192a1b67036afea4a827c886
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2021-01-12 00:15:05 +0200

    Works?!?!? gotta make Reps better.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Arvutus:
-    def init(self, tund):
+    def __init__(self, tund):
         self.tund = tund
         self.hinded = []
 
@@ -14,7 +14,36 @@ class Arvutus:
         return self.hinded
 
     def average_grade(self):
-        retrun
+        return sum(self.hinded) / len(self.hinded)
 
-karlthomas = Arvutus('Matemaatika')
-karlthomas.grades(90, 87, 75, 98)
+    def required_average_grade(self, average_grade, reps):
+        # 1. Keskmine hinne
+        # 2. Hindeid kokku
+        # 3. Mitme tööga vaja
+        # 4. Hinnete protsent kokku
+        #      1.                2.              3.      4.
+        required_average_grade = average_grade * (len(self.hinded) + reps) - sum(self.hinded)
+        if required_average_grade > 100 and reps == 1:
+            print(required_average_grade)
+            return 'Nope'
+
+        elif required_average_grade > 100 and reps == 2:
+            required_average_grade = required_average_grade / 2
+            if required_average_grade > 100:
+                return 'Nope'
+            else:
+                return required_average_grade
+
+        else:
+            return required_average_grade
+
+
+tund1 = Arvutus('Vene keel')
+# 90
+tund1.grades(99, 80, 83, 95)
+# print(tund1.average_grade())
+# print(tund1.required_average_grade(90, 1))
+
+tund2 = Arvutus('Muusika')
+tund2.grades(80, 95, 90)
+print(tund2.required_average_grade(90, 2))
